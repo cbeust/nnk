a2e824b7ec762abeeae9c38fd839188a05d45244
boscs <swan.bosc@outlook.fr>
2017-12-30 23:06:52 +0100

    Update NonLinearities.kt
    added comons linearities

--- src/main/kotlin/com/beust/nnk/NonLinearities.kt
+++ src/main/kotlin/com/beust/nnk/NonLinearities.kt
@@ -4,7 +4,6 @@ data class NonLinearity(
         val activate: ((Float) -> Float),
         val activateDerivative: ((Float) -> Float)
 )
-
 enum class NonLinearities(val value: NonLinearity) {
     TANH(NonLinearity(
             { x -> Math.tanh(x.toDouble()).toFloat() },
@@ -17,5 +16,18 @@ enum class NonLinearities(val value: NonLinearity) {
     LEAKYRELU(NonLinearity(
             { x -> if (x > 0) x else 0.01f * x },
             { x -> if (x > 0) 1f else 0.01f }
-    ))
+    )),
+    SOFTPLUS(NonLinearity(
+            { x -> Math.log10(1 + Math.exp(x.toDouble())).toFloat() },
+            { x -> 1 / (1 + Math.exp(-x.toDouble())).toFloat() }
+    )),
+    SIGMOID(NonLinearity(
+            { x -> 1 / (1 + Math.exp(-x.toDouble())).toFloat() },
+            { x -> Math.exp(x.toDouble() / Math.pow(1 + Math.exp(-x.toDouble()), 2.0)).toFloat() }
+    )),
+    SOFTSIGN(NonLinearity(
+            { x ->  x / (1 + x.absoluteValue) },
+            { x -> 1 / ((1+x.absoluteValue)*(1+x.absoluteValue))}
+    )),
+
 }
