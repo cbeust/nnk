414915cf5af05e256dd375cd02df9f2bcee3f556
boscs <swan.bosc@outlook.fr>
2017-12-30 16:36:27 +0100

    Update NonLinearities.kt
    added relu and leakyrelu

--- src/main/kotlin/com/beust/nnk/NonLinearities.kt
+++ src/main/kotlin/com/beust/nnk/NonLinearities.kt
@@ -9,6 +9,18 @@ object NonLinearities{
     private val CommonNonLinearity= HashMap<String, NonLinearity> ()
     init{
         CommonNonLinearity.put("TANH", NonLinearity({ x->Math.tanh(x.toDouble()).toFloat()},{ x->(1.0f - x * x)}))
+        CommonNonLinearity.put("RELU", 
+                NonLinearity(
+                        { x-> if (x>0){x}else 0f}, 
+                        { x->if (x>0){1f}else 0f}
+                )
+        )
+        CommonNonLinearity.put("LEAKYRELU",
+                NonLinearity(
+                        { x-> if (x>0){x}else {0.01f*x}},
+                        { x->if (x>0){1f}else {0.01f}}
+                )
+        )
     }
 
     fun get(nonLinearityName: String): NonLinearity {
